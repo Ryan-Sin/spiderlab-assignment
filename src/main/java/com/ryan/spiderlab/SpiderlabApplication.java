3008556dcd5a95b1ffa5e0de3f3a142d7f067e26
Ryan-Sin <ryansin937@gmail.com>
2024-03-11 11:33:17 +0900

    first commit

---
+++ src/main/java/com/ryan/spiderlab/SpiderlabApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.spiderlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderlabApplication.class, args);
+	}
+
+}
