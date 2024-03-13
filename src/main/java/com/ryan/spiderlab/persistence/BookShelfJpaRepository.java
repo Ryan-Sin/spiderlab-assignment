66a9343a45df6f1b4f5435596bd83a464e9e1bff
Ryan-Sin <ryansin937@gmail.com>
2024-03-13 23:25:57 +0900

    fix: 불필요한 파일 삭제

--- src/main/java/com/ryan/spiderlab/persistence/BookShelfJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ryan.spiderlab.persistence;
-
-import com.ryan.spiderlab.domain.BookShelf;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookShelfJpaRepository extends JpaRepository<BookShelf, Long> {
-}
